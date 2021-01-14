3104d8d47edc83c42a4cdb3aad237940a1f28cdf
Matjaž Mav <matjaz.mav@gmail.com>
2021-01-14 19:17:29 +0100

    W GAN & interpolation

--- utils/plot.py
+++ utils/plot.py
@@ -3,12 +3,12 @@ from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
 
 
-def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28), title=None, save_path=None):
+def show_tensor_images(image_tensor, num_images=25, size=(1, 28, 28), nrow=5, title=None, save_path=None):
     '''
     Visualizing image grid
     '''
     image_unflat = image_tensor.detach().cpu().view(-1, *size)
-    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    image_grid = make_grid(image_unflat[:num_images], nrow=nrow)
     plt.title(title)
     plt.imshow(image_grid.permute(1, 2, 0).squeeze())
 
